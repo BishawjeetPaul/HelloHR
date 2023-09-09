70c3f5bb6a33e2fd0fc055a10593e816284ff7b7
koushik-hait <koushikhait49@gmail.com>
2023-09-09 19:06:27 +0530

    template added

--- adminPanel/views.py
+++ adminPanel/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'admin-panel/dashboard.html')
+
+
+def login(request):
+    return render(request, 'admin-panel/login.html')
