32cae7e362a0e33dc6f6466c158a6e2794629779
koushik-hait <koushikhait49@gmail.com>
2023-10-02 23:59:43 +0530

    host added

--- HelloHR/settings.py
+++ HelloHR/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = "HelloHR.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hellohr_db',
+        'USER': 'hellohr_user',
+        'PASSWORD': 'hellohr_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -123,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'HelloHR/static/')
 ]
